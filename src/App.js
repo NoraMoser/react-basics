89baeb25820bcff59b008bb0807c9040e57c50f5
Moser <nmoser@ah-1134.corp.arborhealth.com>
2018-02-19 12:29:36 -0600

    input and toggle

--- src/App.js
+++ src/App.js
@@ -4,12 +4,63 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Ashley', age: 33 },
+      { name: 'Jennifer', age: 39}
+    ],
+    showPersons: false
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState( {
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Ashley', age: 33 },
+        { name: 'Jennifer', age: 39}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 33 },
+        { name: 'Jennifer', age: 39}
+      ]
+    })
+  }
+
+  togglePersonsHandler = () => {
+      const doesShow = this.state.showPersons;
+      this.setState({showPersons: !doesShow});
+  }
+
   render() {
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+
+        <div>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Lincoln")}
+          input={this.nameChangedHandler}>My Hobbies: Reading</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        </div>
+      
+      )
+    }
+
     return (
       <div className="App">
-        <Person name='Max' age='28'/>
-        <Person name='Ashley' age='33'>My Hobbies: Reading</Person>
-        <Person name='Jennifer' age='39'/>
+        <button onClick={() => this.togglePersonsHandler()}>Toggle</button>
+        {persons}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => (
-    <div>
-    <p>I'm {props.name} and I am {props.age} years old! {props.children}</p>
+    <div className='Person'>
+    <p onClick={props.click}>I'm {props.name} and I am {props.age} years old! {props.children}</p>
+    <input tyoe='text' onChange={props.input} ></input>
     </div>
 );
+// value={props.name} - for two way binding in the input
 
 export default person;
